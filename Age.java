986d2447e184087d31c8646dd76e0401aefd4e88
Hemanth kumar SB <kumarsbhemanth@gmail.com>
2026-02-19 12:09:37 +0530

    Arrays

---
+++ Age.java
@@ -0,0 +1,14 @@
+class Age
+{
+public static void main(String[]args)
+
+{
+ string footballplayers = {"a","b","c","somethinging"};
+ 
+ for(int i=0; i<=4 ; i++)
+ {
+	 System.out.println("the string names"+footballplayers)
+}
+}
+
+
---
+++ AllFiveRunner - Copy.java
@@ -0,0 +1,18 @@
+class AllFiveRunner{
+	public static void main(String[]anything)
+	{
+		Bnglr.metro("BTM metro");
+		Bnglr.metro(10,"green yellow", true);
+		Cakes.info("chocolate cake", 5);
+		Cakes.info (true, "sri nandi bakery");
+		Chicken.item("chicken  lolipop");
+		Chicken.item(true, 20);
+		Drinks.information("Hot drink coffee", 16);
+		Drinks.information(true, "brown colour");
+		Vehicles.details("xpulse",12000);
+		Vehicles.details("whiteandblue",false,5001);
+	
+	}
+	
+	
+}
