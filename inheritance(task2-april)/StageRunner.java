1136e5cc32ad10daadae625ed8dfb0157471d1e6
Hemanth kumar SB <kumarsbhemanth@gmail.com>
2026-04-02 11:30:57 +0530

    inheritance2-aprl

--- inheritance(task2-april)/StageRunner.java
+++ inheritance(task2-april)/StageRunner.java
@@ -2,19 +2,19 @@ class StageRunner extends StageType
 {
 	public static void main(String[] args)
 	{
-		StageType s1 = new StageType();
-		System.out.println(s1.length);
-		System.out.println(s1.material);
-		System.out.println(s1.isIndoor);
+		StageType stage1 = new StageType();
+		System.out.println(stage1.length);
+		System.out.println(stage1.material);
+		System.out.println(stage1.isIndoor);
 		
-		StageType s2 = new StageType(60,"Steel",true);
-		System.out.println(s2.length);
-		System.out.println(s2.material);
-		System.out.println(s2.isIndoor);
+		StageType stage2 = new StageType(60,"Steel",true);
+		System.out.println(stage2.length);
+		System.out.println(stage2.material);
+		System.out.println(stage2.isIndoor);
 		
-		StageType s3 = new StageType(25.5,"Plastic");
-		System.out.println(s3.length);
-		System.out.println(s3.material);
-		System.out.println(s3.isIndoor);
+		StageType stage3 = new StageType(25.5,"Plastic");
+		System.out.println(stage3.length);
+		System.out.println(stage3.material);
+		System.out.println(stage3.isIndoor);
 	}
 }
--- inheritance(task2-april)/StairRunner.java
+++ inheritance(task2-april)/StairRunner.java
@@ -2,19 +2,19 @@ class StairRunner extends StairType
 {
 	public static void main(String[] args)
 	{
-		StairType st1 = new StairType();
-		System.out.println(st1.steps);
-		System.out.println(st1.type);
-		System.out.println(st1.hasRail);
+		StairType stair1 = new StairType();
+		System.out.println(stair1.steps);
+		System.out.println(stair1.type);
+		System.out.println(stair1.hasRail);
 		
-		StairType st2 = new StairType(20,"Wood",true);
-		System.out.println(st2.steps);
-		System.out.println(st2.type);
-		System.out.println(st2.hasRail);
+		StairType stair2 = new StairType(20,"Wood",true);
+		System.out.println(stair2.steps);
+		System.out.println(stair2.type);
+		System.out.println(stair2.hasRail);
 		
-		StairType st3 = new StairType(10,"Iron");
-		System.out.println(st3.steps);
-		System.out.println(st3.type);
-		System.out.println(st3.hasRail);
+		StairType stair3 = new StairType(10,"Iron");
+		System.out.println(stair3.steps);
+		System.out.println(stair3.type);
+		System.out.println(stair3.hasRail);
 	}
 }
